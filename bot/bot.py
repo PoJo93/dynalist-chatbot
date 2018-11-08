3377cdcc0a071347bf93304477476835ec65d8db
Jonas Pohlmann <jonaspohlmann@yahoo.de>
2018-11-07 16:00:37 -0800

    Send status code of dynalist back

--- bot/bot.py
+++ bot/bot.py
@@ -13,11 +13,10 @@ import json
 
 def post_to_inbox(payload):
     request_content = json.loads(payload.get_data().decode('utf-8'))
-    print(request_content)
+    #print(request_content)
     message = request_content['nlp']['source']
 
     dynalist_token = request_content['conversation']['memory']['token']
-    #dynalist_token = 'WhqJkcbexDdQEl87cA5lEy2hFNIokQkj9iOev-ag7EJAPyUSDipPrzb0Hz6yJ9J6oIbyghDi_t_rWBUlS99CdnFCAixGnjko_4KUfZQpaIG4z0pcHVnWAgu53G1887SF'
     dynalist_payload = {
                           "token": dynalist_token,
                           "index": 0,
@@ -26,7 +25,18 @@ def post_to_inbox(payload):
                           "checked": False
                         }
     r = requests.post('https://dynalist.io/api/v1/inbox/add', json=dynalist_payload)
-    return jsonify(status=200)
+    response_dynalist = r.json()
+    print(response_dynalist)
+    memory_response = request_content['conversation']['memory']
+    memory_response['status_code'] = response_dynalist.get('_code','NoResponse')
+    memory_response['status_message'] = response_dynalist.get('_msg', '')
+    print(memory_response)
+    return jsonify(
+        status=200,
+        conversation={
+            'memory': memory_response
+        }
+    )
 
 
 def check_token_request(payload):
